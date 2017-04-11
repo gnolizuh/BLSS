538f76b5be1c7ef0d0a0d0c6fe9c2c2d69043635
Gino Hu <huzilong_007@163.com>
2017-04-11 22:50:23 +0800

    commit

--- src/rtmp/ngx_rtmp_gop_cache_module.h
+++ src/rtmp/ngx_rtmp_gop_cache_module.h
@@ -42,7 +42,6 @@ struct ngx_rtmp_gop_cache_s {
     ngx_chain_t                        *video_seq_header_data;
     ngx_chain_t                        *audio_seq_header_data;
     ngx_chain_t                        *meta_data;
-    ngx_chain_t                        *meta_data_flv;
     ngx_uint_t                          meta_version;
     ngx_rtmp_header_t                   meta_header;
     ngx_int_t                           video_frame_cnt;
