55694b32f64807276d5f77021b14e5d362a3d64a
Gino Hu <huzilong_007@163.com>
2017-04-12 10:39:10 +0800

    fix complie err.

--- src/rtmp/ngx_http_flv_module.c
+++ src/rtmp/ngx_http_flv_module.c
@@ -7,7 +7,7 @@
 #include <ngx_config.h>
 #include <ngx_core.h>
 #include <ngx_rtmp.h>
-#include "ngx_http_flv_rtmpmodule.h"
+#include "ngx_http_flv_module.h"
 #include "ngx_rtmp_live_module.h"
 #include "ngx_rtmp_codec_module.h"
 
