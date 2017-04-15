816ed1e3335b0efe12af0a5e23d979bcb8d0e77c
Gino Hu <huzilong_007@163.com>
2017-04-15 15:06:43 +0800

    http flv

--- src/rtmp/ngx_rtmp.c
+++ src/rtmp/ngx_rtmp.c
@@ -84,11 +84,11 @@ static char *
 ngx_rtmp_block(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 {
     char                        *rv;
-    ngx_uint_t                   /*i, */m, mi, s;
+    ngx_uint_t                   i, m, mi, s;
     ngx_conf_t                   pcf;
 //    ngx_array_t                  ports;
     ngx_module_t               **modules;
-//    ngx_rtmp_listen_t           *listen;
+    ngx_rtmp_listen_t           *listen;
     ngx_rtmp_module_t           *module;
     ngx_rtmp_conf_ctx_t         *ctx;
     ngx_rtmp_core_srv_conf_t    *cscf, **cscfp;
@@ -318,9 +318,7 @@ ngx_rtmp_block(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
         return NGX_CONF_ERROR;
     }
 
-    return ngx_rtmp_optimize_servers(cf, &cmcf->ports);
-
-    /*if (ngx_array_init(&ports, cf->temp_pool, 4, sizeof(ngx_rtmp_conf_port_t))
+    if (ngx_array_init(&cmcf->ports/*&ports*/, cf->temp_pool, 4, sizeof(ngx_rtmp_conf_port_t))
         != NGX_OK)
     {
         return NGX_CONF_ERROR;
@@ -329,12 +327,12 @@ ngx_rtmp_block(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
     listen = cmcf->listen.elts;
 
     for (i = 0; i < cmcf->listen.nelts; i++) {
-        if (ngx_rtmp_add_ports(cf, &ports, &listen[i]) != NGX_OK) {
+        if (ngx_rtmp_add_ports(cf, &cmcf->ports/*&ports*/, &listen[i]) != NGX_OK) {
             return NGX_CONF_ERROR;
         }
     }
 
-    return ngx_rtmp_optimize_servers(cf, &ports);*/
+    return ngx_rtmp_optimize_servers(cf, &cmcf->ports/*&ports*/);
 }
 
 
