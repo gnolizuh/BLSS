116541878fdc9832c0de90596bd30e7343461c70
Gino Hu <huzilong_007@163.com>
2017-04-15 11:29:48 +0800

    http flv

--- src/rtmp/ngx_rtmp_init.c
+++ src/rtmp/ngx_rtmp_init.c
@@ -34,7 +34,7 @@ ngx_http_flv_init_connection(ngx_http_request_t *r)
 
     ++ngx_http_flv_naccepted;
 
-    conf_port = (ngx_rtmp_conf_port_t *)&ngx_rtmp_core_main_conf->ports.elts[0];
+    conf_port = ngx_rtmp_core_main_conf->ports.elts;
     port = conf_port->ports.elts;
     unix_socket = 0;
 
