0980587754052a7242973f71455dc01c1b4ea704
Gino Hu <huzilong_007@163.com>
2017-04-17 19:54:54 +0800

    commit

--- src/rtmp/ngx_rtmp.h
+++ src/rtmp/ngx_rtmp.h
@@ -382,7 +382,7 @@ typedef struct {
 extern ngx_rtmp_core_main_conf_t   *ngx_rtmp_core_main_conf;
 
 
-typedef struct ngx_rtmp_core_srv_conf_s {
+struct ngx_rtmp_core_srv_conf_s {
     ngx_array_t             applications; /* ngx_rtmp_core_app_conf_t */
 
     ngx_msec_t              timeout;
