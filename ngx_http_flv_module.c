45d2859c3a1b2317f754167a377c5a23d74c3648
Gino Hu <huzilong_007@163.com>
2017-05-12 12:02:51 +0800

    send gop cache after http flv header

--- ngx_http_flv_module.c
+++ ngx_http_flv_module.c
@@ -1154,10 +1154,10 @@ ngx_http_flv_play(ngx_rtmp_session_t *s, ngx_rtmp_play_t *v)
         goto next;
     }
 
-    ngx_log_debug4(NGX_LOG_DEBUG_RTMP, s->connection->log, 0,
-                   "http flv: play: name='%s' start=%uD duration=%uD reset=%d",
-                   v->name, (uint32_t) v->start,
-                   (uint32_t) v->duration, (uint32_t) v->reset);
+    ngx_log_error(NGX_LOG_INFO, s->connection->log, 0,
+                  "http flv: play: name='%s' start=%uD duration=%uD reset=%d",
+                  v->name, (uint32_t) v->start,
+                  (uint32_t) v->duration, (uint32_t) v->reset);
 
     /* join stream as subscriber */
 
