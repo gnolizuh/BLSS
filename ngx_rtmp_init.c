fa32eeca52dc67dd5ff3aef1effdf41b37d8012e
Gino Hu <huzilong_007@163.com>
2017-05-26 23:40:50 +0800

    match http flv connection's host

--- ngx_rtmp_init.c
+++ ngx_rtmp_init.c
@@ -140,7 +140,7 @@ ngx_http_flv_init_connection(ngx_http_request_t *r)
     s->host = r->headers_in.host->value;
     p = ngx_strlchr(s->host.data, s->host.data + s->host.len, ':');
     if (p != NULL) {
-        hosts.len = p - hosts.data;
+        s->host.len = p - s->host.data;
     }
 
     r->read_event_handler = ngx_http_test_reading;
