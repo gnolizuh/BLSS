89f5e04d1933709f440375769be02a8f989910a8
Gino Hu <huzilong_007@163.com>
2017-08-10 11:26:40 +0800

    add_headers command support

--- ngx_http_flv_module.c
+++ ngx_http_flv_module.c
@@ -559,6 +559,10 @@ ngx_http_flv_http_handler(ngx_http_request_t *r)
     cln->handler = ngx_http_flv_cleanup;
     cln->data = r;
 
+    r->headers_out.status = NGX_HTTP_OK;
+
+    ngx_http_flv_headers_filter(r);
+
     return NGX_OK;
 }
 
@@ -713,8 +717,6 @@ ngx_http_flv_http_send_header(ngx_rtmp_session_t *s, ngx_rtmp_session_t *ps)
     u_char                          flv_header[] = "FLV\x1\0\0\0\0\x9\0\0\0\0";
 
     r = s->connection->data;
-
-    r->headers_out.status = NGX_HTTP_OK;
 
     if (r->header_sent) {
         return;
