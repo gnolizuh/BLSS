f9f25ea186a0e876e08d2f72d637899d3b94a4a1
Gino Hu <huzilong_007@163.com>
2018-07-31 11:16:18 +0800

    modulo negative number may cause action undefined

--- ngx_rtmp_handler.c
+++ ngx_rtmp_handler.c
@@ -829,7 +829,8 @@ ngx_rtmp_send_message(ngx_rtmp_session_t *s, ngx_chain_t *out,
 {
     ngx_uint_t                      nmsg;
 
-    nmsg = (s->out_last - s->out_pos) % s->out_queue + 1;
+    // modulo negative number may cause action undefined. 
+    nmsg = (s->out_last - s->out_pos + s->out_queue) % s->out_queue + 1;
 
     if (priority > 3) {
         priority = 3;
